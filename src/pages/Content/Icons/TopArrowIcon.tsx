edbd0041e267ac29cd8d6090f0242953559b6f16
Christophe DELAFORGE <christophe.delaforge@almaviacx.com>
2023-06-14 09:44:16 +0200

    Cleanup and misc debug

---
+++ src/pages/Content/Icons/TopArrowIcon.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const TopArrowIcon = () => {
+  return (
+    <svg width="32" height="32" viewBox="-60 0 640 540">
+      <g xmlns="http://www.w3.org/2000/svg" transform="translate(0,-540.3622)">
+        <path fill="#222222" stroke="#222222" strokeWidth={38.88000107} d="M 439.28228,860.51096 256.00063,677.22934 72.71772,860.51096 l 54.98539,54.9841 128.29752,-128.29752 128.29622,128.29752 z" id="path3766-1" inkscape:connector-curvature="0" xmlns:sodipodi="http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd" sodipodi:nodetypes="ccccccc" />
+      </g>
+    </svg>
+  );
+};
+
+export default TopArrowIcon;
--- src/pages/Content/SideMenu.tsx
+++ src/pages/Content/SideMenu.tsx
@@ -5,7 +5,8 @@ import { Game } from "../../config/Configuration";
 import SideMenuItem from "./SideMenuItem";
 import PlayerIcon from "./PlayerIcon";
 import CloseIcon from "./Icons/CloseIcon";
-import SandwichIcon from "./Icons/SandwitchIcon";
+import SandwichIcon from "./Icons/SandwichIcon";
+import TopArrowIcon from "./Icons/TopArrowIcon";
 import Avatar from "./Avatar";
 import { Player } from "./Misc";
 
@@ -14,16 +15,6 @@ const Container = styled.div`
   flex-flow: column;
   gap: 0.8em;
 `;
-
-const TopArrowIcon = () => {
-  return (
-    <svg width="32" height="32" viewBox="-60 0 640 540">
-      <g xmlns="http://www.w3.org/2000/svg" transform="translate(0,-540.3622)">
-        <path fill="#222222" stroke="#222222" strokeWidth={38.88000107} d="M 439.28228,860.51096 256.00063,677.22934 72.71772,860.51096 l 54.98539,54.9841 128.29752,-128.29752 128.29622,128.29752 z" id="path3766-1" inkscape:connector-curvature="0" xmlns:sodipodi="http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd" sodipodi:nodetypes="ccccccc" />
-      </g>
-    </svg>
-  );
-};
 
 interface SideMenuProps {
   players: [Player],
